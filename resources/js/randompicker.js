b6e07356b6fb9351d0f6e3c48b7540807345de08
Drobniuch <drobniuch@gmail.com>
2021-02-20 09:52:51 +0100

    bigger question bubble; I have a question; delayed blah

--- resources/js/randompicker.js
+++ resources/js/randompicker.js
@@ -9,15 +9,15 @@ var chromaticScale = [
     "How to run a mob programming session remotely?",
     "How to introduce TDD and BDD approach in your team?",
     "How to move from acceptance criteria towards specification by example?",
-    "How to involve testers in the initial stages of software development and be like the ​​three amigos?",
+    "How to involve testers in the initial stages of software development?",
     "How to talk with users and stakeholders to uncover hidden assumptions?",
     "How to work online collaboratively on an outcome based product backlog?",
-    "How to run an example mapping session remotely  and create a sprint backlog with examples and wireframes?",
+    "How to run an example mapping session remotely?",
     "How to document your product using live executable documentation in Gherkin?"
 ]
 
 var notePool = [];
-var beat = 4;
+var beat = 2;
 
 function myTimer() {
   if (beat==0) {
