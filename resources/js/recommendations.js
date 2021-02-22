cc9ed7909f76319f9358c9809f95c473e7277976
Drobniuch <drobniuch@gmail.com>
2021-02-22 21:33:40 +0100

    some more recomendations

--- resources/js/recommendations.js
+++ resources/js/recommendations.js
@@ -12,9 +12,12 @@ var chromaticScale = [
     '"Great training, a lot of knowledge"',
     '"A great way to conduct online training - breakeout rooms, working together in groups. A very thoughtful way to conduct workshops. Orderly material, logically arranged. The whole thing was dynamic and very interesting. Great that there was more practice than theory."',
     '"Very positive approach, great involvement of trainers"',
-    '"Interesting approach (not only a good approach to recruitment, but also harmful techniques)"',
+    '"Interesting approach (I liked not only the good approach to recruitment, but also the part about harmful techniques)"',
     '"Great materials and tools used for training"',
     '"THANK YOU VERY MUCH <3 <3 <3 you are great!"',
+    '"They should send everyone from our company to your training :)"',
+    '"I like the use of mindmap to analyze the backlog. Respect for making that powerpoint. A good script."'
+
 
 ]
 
